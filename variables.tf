246c4d3c4d5bd84c016031a1956961741334e169
donmasters <don.masters@digital.justice.gov.uk>
2021-11-30 09:03:04 +0000

    create-initial-module-terraform-code (#3)
    * create-initial-module-terraform-code
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "transit" {
+  type = string
+}
+
+variable "protected" {
+  type = string
+}
+
+variable "subnet_sets" {
+  type = map(any)
+}
+
+variable "tags_common" {
+  description = "MOJ required tags"
+  type        = map(string)
+}
+
+variable "tags_prefix" {
+  description = "prefix for name tags"
+  type        = string
+}
+
+variable "transit_gateway_id" {
+  description = "tgw ID"
+  type        = string
+}
+
+variable "additional_endpoints" {
+  description = "additional endpoints required for VPC"
+  type        = list(any)
+}
+
+variable "bastion_linux" {
+  description = ""
+  type        = bool
+  default     = false
+}
+
+variable "bastion_windows" {
+  description = ""
+  type        = bool
+  default     = false
+}
+
+variable "vpc_flow_log_iam_role" {
+  description = "VPC Flow Log IAM role ARN for VPC Flow Logs to CloudWatch"
+  type        = string
+}
