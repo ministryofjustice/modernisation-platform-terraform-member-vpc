5d4f3cfd56eb400484958637dbfdd981484a5931
Aaron Robinson <41325732+ASTRobinson@users.noreply.github.com>
2024-05-20 16:12:31 +0100

    Update versions.tf

--- test/unit-test/versions.tf
+++ test/unit-test/versions.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.4"
     }
+    http = {
+      source = "hashicorp/http"
+      version = "~> 3.3"
+    }
   }
-  required_version = ">= 1.0.1"
+  required_version = "~> 1.0"
 }
