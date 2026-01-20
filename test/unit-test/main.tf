2527f1b8670ef759ec06f1e08167abb1e1ad146c
Aaron Robinson <aaron.robinson1@justice.gov.uk>
2026-01-20 00:57:51 +0000

    fix unit tests

--- test/unit-test/main.tf
+++ test/unit-test/main.tf
@@ -352,11 +352,4 @@ resource "aws_route_table_association" "secondary_cidr_subnets" {
   subnet_id      = each.value.id
 }
 
-# Network ACL associations for secondary CIDR subnets
-# Secondary subnets use the same NACLs as their corresponding primary subnet type
-resource "aws_network_acl_association" "secondary_cidr_subnets" {
-  for_each = aws_subnet.secondary_cidr_subnets
-
-  network_acl_id = local.secondary_cidr_subnets_with_keys[each.key].type == "protected" ? aws_network_acl.protected.id : aws_network_acl.nacl[local.secondary_cidr_subnets_with_keys[each.key].type].id
-  subnet_id      = each.value.id
-}
+# Note: NACL associations not included in test as test uses inline resources without NACLs
